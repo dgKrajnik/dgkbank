e8358f19d41815ba41035887d69679413ef3e47f
Daniel Krajnik <dgkrajnik@gmail.com>
2018-01-22 15:11:04 +1100

    Add development CORS setting.

--- spring-server/src/main/kotlin/com.dgkrajnik.bank.server/Server.kt
+++ spring-server/src/main/kotlin/com.dgkrajnik.bank.server/Server.kt
@@ -3,6 +3,10 @@ package com.dgkrajnik.bank.server
 import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
@@ -20,4 +24,14 @@ fun main(args: Array<String>) {
     app.setBannerMode(Banner.Mode.OFF)
     app.isWebEnvironment = true
     app.run(*args)
+}
+
+@Bean
+fun corsConfigurer(): WebMvcConfigurer {
+    return object : WebMvcConfigurerAdapter() {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            //TODO: CHANGE THIS, WOW.
+            registry.addMapping("/dgkbank/*").allowedOrigins("*")
+        }
+    };
 }
